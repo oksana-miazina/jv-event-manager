804a5dbe5d6c8a092b90b1953b81471f83867c9c
Oksana Miazina <oksana.myazina@gmail.com>
2024-06-20 22:57:47 +0300

    feat: implement EventManager

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,26 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+public class EventManager {
+    private final List<EventListener> listeners = new CopyOnWriteArrayList<>();
+
+    public void registerListener(EventListener listener) {
+        listeners.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener listener : listeners) {
+            listener.onEvent(event);
+        }
     }
 
     public void shutdown() {
-
+        listeners.clear();
     }
 }
